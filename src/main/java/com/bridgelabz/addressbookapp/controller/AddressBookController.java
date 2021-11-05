54e39e330d94dec790868f9570000a73926e0201
Aravindhan V <aravindhan.v@perfios.com>
2021-11-05 15:02:52 +0530

    ADDED: Handler to get contact by id

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookapp.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class AddressBookController {
 	@RequestMapping(value= {"","/"})
 	public String welcomeUser() {
 		return "Welcome to address book home";
+	}
+	
+	@RequestMapping("/get/{id}")
+	public String welcomeSpecificUser(@PathVariable String id) {
+		return "Welcome, User "+id;
 	}
 }
