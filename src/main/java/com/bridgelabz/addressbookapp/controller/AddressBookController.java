5badb774280258d08c167de81b2db88473ec444b
Aravindhan V <aravindhan.v@perfios.com>
2021-11-05 15:18:32 +0530

    ADDED: POST handler to insert contact

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.addressbookapp.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.addressbookapp.model.Contact;
 
 @RestController
 @RequestMapping("/addressbookservice")
 public class AddressBookController {
-	@RequestMapping(value= {"","/"})
+	@RequestMapping(value= {"","/","/get"})
 	public String welcomeUser() {
 		return "Welcome to address book home";
 	}
@@ -15,5 +18,10 @@ public class AddressBookController {
 	@RequestMapping("/get/{id}")
 	public String welcomeSpecificUser(@PathVariable String id) {
 		return "Welcome, User "+id;
+	}
+	
+	@RequestMapping("/post")
+	public String createContact(@RequestBody Contact contact) {
+		return "Added "+contact.getName()+" to list";
 	}
 }
