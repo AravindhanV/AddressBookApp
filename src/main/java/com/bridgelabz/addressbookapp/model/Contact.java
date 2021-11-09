7cd8ac72faa8d30870c8d9547ae2e286f4691ac9
Aravindhan V <aravindhan.v@perfios.com>
2021-11-09 07:27:01 +0530

    ADDED: table creation using @entity

--- src/main/java/com/bridgelabz/addressbookapp/model/Contact.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/Contact.java
@@ -1,10 +1,22 @@
 package com.bridgelabz.addressbookapp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "contact")
 public @Data class Contact {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="contact_id")
 	private String id;
 	private String name;
 	
